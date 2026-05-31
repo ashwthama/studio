b955829fd5d90e737bea7ef6b4b935ae0f7294d2
ashwthama <ashwanikumarnt@gmail.com>
2026-05-31 09:55:17 +0530

    update next.js version

--- src/types/project.d.ts
+++ src/types/project.d.ts
@@ -2,7 +2,3 @@ declare module '*.jpg' {
   const src: import('next/image').StaticImageData;
   export default src;
 }
-
-declare module '@hookform/resolvers/zod/dist/zod.js' {
-  export const zodResolver: any;
-}
